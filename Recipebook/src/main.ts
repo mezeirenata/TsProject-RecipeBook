f2a53d49436eaecd26c9cb6e6ba12e02e4e3031a
Renáta Mezei <mezei.renata@students.jedlik.eu>
2026-05-18 01:01:29 +0200

    base: bővítendő oldalak

---
+++ Recipebook/src/main.ts
@@ -0,0 +1,11 @@
+import './styles/style.css';
+import './styles/ingredients.css'
+window.addEventListener('scroll', () => {
+    let scrolled = false ;
+    if (window.scrollY > 50){
+        scrolled = true;
+    }
+    document.getElementById("navbar")!.classList.toggle('scrolled',scrolled);
+    document.getElementById("title")!.classList.toggle('scrolled',scrolled);
+     
+});
